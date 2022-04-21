18ac820c1096689cab52eb033d239d8dfc016288
Cesar Calva <cesar.cruz@acftechnologies.com>
2022-04-20 21:54:13 -0500

    End of sesion 9

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { IProduct } from './models/product';
-import { IPagination } from "./models/pagination";
 
 @Component({
   selector: 'app-root',
@@ -10,18 +8,11 @@ import { IPagination } from "./models/pagination";
 })
 export class AppComponent implements OnInit {
   title = 'client';
-  products: IProduct[];
 
   constructor(private http: HttpClient){}
 
   ngOnInit(): void {
-    this.http.get("https://localhost:5001/api/products?pageSize=50").subscribe(
-      (response: IPagination) => {
-      console.log(response);
-      this.products = response.data;
-    }, error => {
-      console.log(error);
-    });
+    
   }
   
 }
