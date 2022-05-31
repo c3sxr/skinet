97eb17e94b2b70b1b6f11bb07398a40061a06674
Cesar Calva <cesar.cruz@acftechnologies.com>
2022-05-30 20:16:22 -0500

    End of section 14

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { BasketService } from './basket/basket.service';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'client';
 
-  constructor(private http: HttpClient){}
+  constructor(private basketService: BasketService){}
 
   ngOnInit(): void {
-    
+    const basketId = localStorage.getItem('basket_id');
+    if (basketId) {
+      this.basketService.getBasket(basketId).subscribe({
+        next: () => {
+          console.log('initialized basket');
+        }, error: error =>{
+          console.log(error);
+        }
+      })
+    }
   }
   
 }
