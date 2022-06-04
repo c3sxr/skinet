084a9c502bf8f77b49029b2af07a361a1d6b7d16
Cesar Calva <cesar.cruz@acftechnologies.com>
2022-06-04 14:13:31 -0500

    End of section 16

--- API/Extensions/SwaggerServiceExtensions.cs
+++ API/Extensions/SwaggerServiceExtensions.cs
@@ -1,10 +1,32 @@
+using Microsoft.OpenApi.Models;
+
 namespace API.Extensions;
 
 public static class SwaggerServiceExtensions
 {
     public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services)
     {
-        services.AddSwaggerGen();
+        services.AddSwaggerGen(c => {
+            var securitySchema = new OpenApiSecurityScheme
+            {
+                Description = "JWT Auth Bearer Scheme",
+                Name = "Authorization",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.Http,
+                Scheme = "bearer",
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            };
+
+            c.AddSecurityDefinition("Bearer", securitySchema);
+            var securityRequirements = new OpenApiSecurityRequirement{{securitySchema, new []{
+                "Bearer"
+            }}};
+            c.AddSecurityRequirement(securityRequirements);
+        });
         return services;
     }
 
--- Core/Entities/Identity/Address.cs
+++ Core/Entities/Identity/Address.cs
@@ -1,20 +1,26 @@
 using System.ComponentModel.DataAnnotations;
 
-namespace API.Dtos;
-
-public class AddressDto
+namespace Core.Entities.Identity
 {
-        [Required]
+    public class Address
+    {
+        public int Id { get; set; }
+
         public string FirstName { get; set; }
-        [Required]
+
         public string LastName { get; set; }
-        [Required]
+
         public string Street { get; set; }
-        [Required]
+
         public string City { get; set; }
-        [Required]
+
         public string State { get; set; }
-        [Required]
+
         public string ZipCode { get; set; }
 
+        [Required]
+        public string AppUserId { get; set; }
+
+        public AppUser AppUser { get; set; }
+    }
 }
