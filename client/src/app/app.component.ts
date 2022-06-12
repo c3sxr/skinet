10ab9f952bc0547b68a8b3a56a40af9e2f286c7e
Cesar Calva <cesar.cruz@acftechnologies.com>
2022-06-11 19:45:20 -0500

    End of section 17

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from './account/account.service';
 import { BasketService } from './basket/basket.service';
 
 @Component({
@@ -9,16 +10,35 @@ import { BasketService } from './basket/basket.service';
 export class AppComponent implements OnInit {
   title = 'client';
 
-  constructor(private basketService: BasketService){}
+  constructor(private basketService: BasketService, private accountService: AccountService){}
 
   ngOnInit(): void {
+    this.loadBasket();
+    this.loadCurrentUser();
+  }
+
+  loadCurrentUser(){
+    const token = localStorage.getItem('token');
+ 
+      this.accountService.loadCurrentUser(token).subscribe({
+        next: () =>{
+          console.log('loaded user');
+        },
+        error: error =>{
+          console.log(error);
+        }
+      });
+    
+  }
+
+  loadBasket(){
     const basketId = localStorage.getItem('basket_id');
     if (basketId) {
       this.basketService.getBasket(basketId).subscribe({
         next: () => {
           console.log('initialized basket');
-        }, error: error =>{
-          console.log(error);
+        }, error: ex =>{
+          console.log(ex);
         }
       })
     }
